da7350822df480337fb96829b6afe9dc475bed9d
Lucas Simas <simas.lucas@hotmail.com>
2024-05-16 15:56:42 +0100

    fix: Avoid duplicated itens on list and indentation

--- Fluent.CodeGen/ClassGen.cs
+++ Fluent.CodeGen/ClassGen.cs
@@ -3,6 +3,7 @@ using System.CodeDom.Compiler;
 using System.IO;
 using System.Collections.Generic;
 using Fluent.CodeGen.Consts;
+using System.Linq;
 
 namespace Fluent.CodeGen
 {
@@ -12,8 +13,8 @@ namespace Fluent.CodeGen
         public string ClassName { get; private set; }
         private string? extends;
         private bool isStatic;
-        private List<string> implements;
-        private List<string> namespaces;
+        private HashSet<string> implements;
+        private HashSet<string> namespaces;
         private string accessModifier = AccessModifiers.Default;
 
         private List<MethodGen> methods;
@@ -21,20 +22,26 @@ namespace Fluent.CodeGen
         public ClassGen(string name)
         {
             ClassName = name;
-            implements = new List<string>();
-            namespaces = new List<string>();
+            implements = new HashSet<string>();
+            namespaces = new HashSet<string>();
             methods = new List<MethodGen>();
         }
 
         public ClassGen Using(params string[] namespaces)
         {
-            this.namespaces.AddRange(namespaces);
+            foreach(var @namespace in namespaces)
+            {
+                this.namespaces.Add(@namespace);
+            }
             return this;
         }
 
-        public ClassGen Implements(params string[] @using)
+        public ClassGen Implements(params string[] usings)
         {
-            this.implements.AddRange(@using);
+            foreach (var @using in usings)
+            {
+                this.implements.Add(@using);
+            }
             return this;
         }
 
@@ -79,6 +86,11 @@ namespace Fluent.CodeGen
             foreach (string @namespace in namespaces)
             {
                 this.indentedTextWriter.WriteLine($"using {@namespace};");
+            }
+
+            if(namespaces.Any())
+            {
+                this.indentedTextWriter.WriteLine();
             }
 
             if (!string.IsNullOrEmpty(@namespace))
@@ -136,9 +148,18 @@ namespace Fluent.CodeGen
             this.indentedTextWriter.WriteLine(classDeclaration.ToString());
             this.indentedTextWriter.WriteLine("{");
             this.indentedTextWriter.Indent++;
+
+            var last = methods.LastOrDefault();
             methods.ForEach(method => 
             {
                 this.indentedTextWriter.WriteLine(method.GenerateCode(this.indentedTextWriter.Indent));
+                if(!last.Equals(method))
+                {
+                    var indentation = this.indentedTextWriter.Indent;
+                    this.indentedTextWriter.Indent = 0;
+                    this.indentedTextWriter.WriteLine();
+                    this.indentedTextWriter.Indent = indentation;
+                }
             });
 
             this.indentedTextWriter.Indent--;
